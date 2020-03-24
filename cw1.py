f9fc6e19b882c83329b17b3a91dccc823fd31835
laszukk <59913195+laszukk@users.noreply.github.com>
2020-03-24 14:01:56 +0100

    Add files via upload

---
+++ cw1.py
@@ -0,0 +1,46 @@
+import math
+a =1
+b =2
+c ="napis"
+print(c)
+print(b/a)
+print(a*b)
+print(a+b)
+print(a-b)
+print(b%a)
+print(b**b)
+print(math.e**10)
+print(math.exp(5+math.sin(8)**2))
+print(math.fabs(3.55))
+
+imie = u"KACPER"
+nazwisko = u"LASZUK"
+print(imie.capitalize(),nazwisko.capitalize())
+
+piosenka= "cos tam aifafn ajsf aaa da da gtsdgfgdf da da da"
+print(piosenka.count("da"))
+
+print(imie[0],imie[-1])
+print(imie[:2],imie[2:])
+
+print(math.e)
+
+lista=["Donnie Darko","Parasite","Joker"]
+lista.sort()
+print("Nowa lista po zmianach:\n",lista)
+
+alfa = [ math.pi/3,math.pi/4,math.pi/6]
+print(math.sin(alfa[0]))
+
+zdanie= u"konstantynopolitańczykowianeczka"
+listak=[]
+
+listak.insert(0,zdanie[:10])
+listak.insert(1,zdanie[10:])
+print(listak)
+
+kontakty ={}
+kontakty['Frajer'] = 'Jacek Broz'
+print(kontakty['Frajer'])
+
+print(len(kontakty))
